649dc4d780dda9e60da7737b31362cd2f8e046ad
AlexandrKoba <babakoba@yandex.ru>
2023-08-15 02:21:17 +0700

    "Otlichno my friend RUN"

--- bot.py
+++ bot.py
@@ -5,9 +5,6 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
-
-
-
 import sqlite3
 import re
 
@@ -30,11 +27,23 @@ async def get_sorts_from_db():
     conn.close()
     return rows
 
+async def sort_filter(callback_query: types.CallbackQuery):
+    sorts = await get_sorts_from_db()
+    return callback_query.data and callback_query.data in [row[0] for row in sorts]
+
 @dp.message_handler(Command("start"))
 async def cmd_start(message: types.Message):
+    greeting_text = "Доставка Шишек по острову Панган!"
+    await message.answer(greeting_text)
+
+    sorts = await get_sorts_from_db()
+    for sort_name, description, photo_path in sorts:
+        sort_info = f"{sort_name}\nОписание: {description}"
+        await message.answer_photo(photo=open(photo_path, 'rb'), caption=sort_info)
+
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(KeyboardButton("Заказать"))
-    await message.answer("Приветствие!", reply_markup=markup)
+    await message.answer("Выберите действие:", reply_markup=markup)
 
 @dp.message_handler(lambda message: message.text == 'Заказать')
 async def choose_sort(message: types.Message):
@@ -45,7 +54,7 @@ async def choose_sort(message: types.Message):
     await message.answer("Выберите сорт:", reply_markup=markup)
     await SortOrder.choosing_sort.set()
 
-@dp.callback_query_handler(lambda c: c.data and c.data in [row[0] for row in get_sorts_from_db()], state=SortOrder.choosing_sort)
+@dp.callback_query_handler(sort_filter, state=SortOrder.choosing_sort)
 async def process_sort_chosen(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, f"Вы выбрали {callback_query.data}. Сколько грамм?")
@@ -56,7 +65,6 @@ async def process_sort_chosen(callback_query: types.CallbackQuery, state: FSMCon
 async def process_grams(message: types.Message, state: FSMContext):
     grams = int(message.text)
     order_data = await state.get_data()
-    # Запись данных в вашу БД здесь...
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.execute("INSERT INTO orders (customer, sort_order, gramm_order) VALUES (?, ?, ?)", (message.from_user.id, order_data['sort'], grams))
